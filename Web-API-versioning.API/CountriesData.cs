7d6c5472631ee8fda88d9b70e2c01c3f5099f212
supriyamugeraya <mugerayasupriya@gmail.com>
2025-09-10 10:55:06 +0530

    first commit

---
+++ Web-API-versioning.API/CountriesData.cs
@@ -0,0 +1,26 @@
+﻿using Web_API_versioning.API.Models.Domain;
+
+namespace Web_API_versioning.API
+{
+    public static class CountriesData
+    {
+        public static List<Country> Get()
+        {
+            var Countries = new[]
+            {
+                new {Id =1, Name ="United States"},
+                new {Id =2, Name ="Germany"},
+                new {Id =3, Name ="Brazil"},
+                new {Id =4, Name ="China"},
+                new {Id =5, Name ="Italy" },
+                new {Id =7, Name ="Mexico"},
+                new {Id =6, Name ="South Africa"},
+                new {Id =8, Name ="Japan"},
+                new {Id =9, Name ="Russia"},
+                new {Id =10, Name ="Australia" }
+
+           };
+            return Countries.Select(c=>new Country{Id=c.Id,Name=c.Name}).ToList();
+        }
+    }
+}
---
+++ Web-API-versioning.API/Models/Domain/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace Web_API_versioning.API.Models.Domain
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
